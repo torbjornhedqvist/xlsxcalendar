f98f2586d5379aea8833064e39033697b6eb8983
Torbjörn Hedqvist <torbjorn.hedqvist@ericsson.com>
2025-12-16 14:06:59 +0100

    Minor color changes.

--- xlsxcalendar_nicegui.py
+++ xlsxcalendar_nicegui.py
@@ -63,8 +63,21 @@ def main_page():
 
     ui.label('XlsxCalendar Configuration').classes('text-h4 q-mb-md')
 
+    def close_all_sections():
+        """Close all expanded sections"""
+        dates_expansion.value = False
+        worksheet_expansion.value = False
+        content_expansion.value = False
+        holidays_expansion.value = False
+        themes_expansion.value = False
+        import_expansion.value = False
+        output_expansion.value = False
+
+    ui.button('Close All Sections', on_click=close_all_sections).classes('q-mb-md')
+
     with ui.card().classes('w-full max-w-4xl').style('background-color: #F0F8FF'):
-        with ui.expansion('Dates', icon='calendar_today').classes('w-full'):
+        dates_expansion = ui.expansion('Dates', icon='calendar_today').classes('w-full')
+        with dates_expansion:
             ui.label('Provide the start and end date of the complete calendar to be '
                      'generated. Note! If you are using an importer module ensure the '
                      'dates are matching.').classes('text-caption text-blue-8 q-mb-sm')
@@ -72,37 +85,71 @@ def main_page():
                                   value=config.get('start_date', '')).props('type=date')
             end_date = ui.input('End Date',
                                 value=config.get('end_date', '')).props('type=date')
+            ui.separator().style('background-color: #003366; height: 2px;')
 
-        with ui.expansion('Worksheet Internal Settings',
-                          icon='description').classes('w-full'):
-            ui.label('Information section').classes('text-caption text-grey-6 q-mb-sm')
+        worksheet_expansion = ui.expansion('Worksheet Internal Settings',
+                          icon='description').classes('w-full')
+        with worksheet_expansion:
+            ui.label('Inside the Excel, just the name of the worksheet, defaults to '
+                     '- Calendar -').classes('text-caption text-blue-8 q-mb-sm')
             worksheet_name = ui.input('Worksheet Name',
                                       value=config.get('worksheet_name', ''),
-                                      placeholder='- Calendar -').classes('w-full')
+                                      placeholder='- Calendar -').classes('w-full').style(
+                                          f'font-size: 100%; background-color: {TEXTBOX_COLOR}')
+            
+            # ui.separator().style('background-color: #A0A0A0; height: 1px;')
+
+            ui.label('And the color of the Worksheet tab. Click on the line to the left of '
+                     'the currently configured color to change.').classes(
+                     'text-caption text-blue-8 q-mb-sm')
             with ui.row().classes('w-full items-center'):
-                ui.label('Tab Color:').classes('w-32')
+                ui.label('Worksheet Tab Color:').classes('w-32')
                 worksheet_tab_color = ui.input('',
                                                value=config.get('worksheet_tab_color',
                                                                 '#ff9966')).props(
                                                    'type=color').classes('w-20')
+                color_preview = ui.label('').classes('w-8 h-8 rounded border').style(
+                    f'background-color: {config.get("worksheet_tab_color", "#ff9966")}')
+                
+                worksheet_tab_color.on_value_change(lambda e: color_preview.style(
+                    f'background-color: {e.value}'))
+                
+            ui.separator().style('background-color: #A0A0A0; height: 1px;')
+            ui.label('The language of the day of the week.').classes(
+                'text-caption text-blue-8 q-mb-sm')
             with ui.row().classes('w-full items-center'):
                 ui.label('Day Language:').classes('w-32')
-                language = ui.select(['en', 'sv', 'fi', 'es'],
-                                     value=config.get('worksheet_day_of_week_language',
-                                                      'en')).classes('w-32')
+                language = ui.radio(['en', 'sv', 'fi', 'es'],
+                                    value=config.get('worksheet_day_of_week_language', 'en'))
+            ui.separator().style('background-color: #003366; height: 2px;')
 
-        with ui.expansion('Content Settings', icon='edit').classes('w-full'):
-            ui.label('Information section').classes('text-caption text-grey-6 q-mb-sm')
+        content_expansion = ui.expansion('Content Settings', icon='edit').classes('w-full')
+        with content_expansion:
+            ui.label('The heading in the left-most row of the calendar where you could '
+                     'put your team name, activity or any other smart heading. '
+                     'In Content Entries you can put a list of names or activities to '
+                     'be put under the heading. Note! this can be overridden by an '
+                     'importer plugin.').classes('text-caption text-blue-8 q-mb-sm')
             content_heading = ui.input('Content Heading',
                                        value=config.get('content_heading', ''),
                                        placeholder='Team/Activity').classes('w-full').style(
                                            f'font-size: 100%; background-color: {TEXTBOX_COLOR}')
             content_entries = ui.textarea('Content Entries (one per line)',
                                           value='\n'.join(config.get('content_entries', []))
-                                          if config.get('content_entries') else '')
+                                          if config.get('content_entries') else '').classes(
+                                              'w-full').style(
+                                              f'background-color: {TEXTBOX_COLOR}')
+            ui.separator().style('background-color: #003366; height: 2px;')
 
-        with ui.expansion('Holidays', icon='event').classes('w-full'):
-            ui.label('Information section').classes('text-caption text-grey-6 q-mb-sm')
+        holidays_expansion = ui.expansion('Holidays', icon='event').classes('w-full')
+        with holidays_expansion:
+            ui.label('Predefined holiday schedules. The dates specified in these files will be '
+                     'marked with the weekend color and get a special note below calendar in '
+                     'the column for the specific day, indicated by an exclamation character '
+                     '[!] with the associated text. Multiple selections possible. '
+                     'If there is a conflict in dates with Custom Holidays below, the '
+                     'Custom Holidays take precedence.').classes(
+                         'text-caption text-blue-8 q-mb-sm')
             ui.label('Holiday Files:')
             holiday_checkboxes = []
             for holiday_file in holiday_files:
@@ -111,21 +158,34 @@ def main_page():
                 checkbox = ui.checkbox(holiday_file, value=checked)
                 holiday_checkboxes.append((checkbox, holiday_file))
 
-            custom_holidays = ui.textarea('Custom Holidays (YYYY-MM-DD: Description)',
-                                          value='\n'.join([f'{k}: {v}' for k, v in
-                                                           config.get('holidays',
-                                                                      {}).items()])).classes(
-                                                'w-full').style(
-                                                    f'background-color: {TEXTBOX_COLOR}')
+            ui.separator().style('background-color: #A0A0A0; height: 1px;')
+            ui.label('Customized holiday days which is not part of any standard holiday template '
+                     'from the selection above. It can be any special days or non-business days '
+                     'which is happening any day in the week.').classes(
+                         'text-caption text-blue-8 q-mb-sm')
+            ui.label('Example: 2025-12-11: \'Bob\\\'s birthday\'').classes(
+                     'text-caption text-blue-8 q-mb-sm')
+            custom_holidays = ui.textarea('Custom Holidays, one per line',
+                    value='\n'.join([f'{k}: {v}' for k, v in config.get('holidays',
+                        {}).items()])).classes('w-full').style(
+                            f'background-color: {TEXTBOX_COLOR}')
+            ui.separator().style('background-color: #003366; height: 2px;')
 
-        with ui.expansion('Themes', icon='palette').classes('w-full'):
-            ui.label('Information section').classes('text-caption text-grey-6 q-mb-sm')
-            theme_radio = ui.radio(['None'] + theme_files,
+        themes_expansion = ui.expansion('Themes', icon='palette').classes('w-full')
+        with themes_expansion:
+            ui.label('Various pre-defined color themes for the calendars').classes(
+                'text-caption text-blue-8 q-mb-sm')
+            theme_radio = ui.radio(['None (default settings)'] + theme_files,
                                    value='None' if not config.get('theme_imports')
                                    else config.get('theme_imports'))
+            ui.separator().style('background-color: #003366; height: 2px;')
 
-        with ui.expansion('Data Import', icon='upload').classes('w-full'):
-            ui.label('Information section').classes('text-caption text-grey-6 q-mb-sm')
+        import_expansion = ui.expansion('Data Import', icon='upload').classes('w-full')
+        with import_expansion:
+            ui.label('Plugins which takes an importer file as input and populates the calendar '
+                     'with data. Can be highly specialized and you need to read the README '
+                     'for the plugin to understand how it is supposed to be used.').classes(
+                'text-caption text-blue-8 q-mb-sm')
             ui.label('Plugins')
             importer_module = ui.radio(['None', 'plugins.ess_importer'],
                                        value='None' if not config.get('importer_module')
@@ -152,8 +212,10 @@ def main_page():
                         importer_file.value = file_path
 
                 ui.button('Browse', on_click=browse_file).classes('w-20')
+            ui.separator().style('background-color: #003366; height: 2px;')
 
-        with ui.expansion('Output', icon='save').classes('w-full'):
+        output_expansion = ui.expansion('Output', icon='save').classes('w-full')
+        with output_expansion:
             ui.label('Provide the file name of the Excel file which will contain the '
                      'results in Output File below. The file will be placed in your '
                      'Downloads folder.').classes('text-caption text-blue-8 q-mb-sm')
@@ -161,6 +223,7 @@ def main_page():
                                    value=config.get('output_file', ''),
                                    placeholder='./output.xlsx').classes('w-full').style(
                                        f'font-size: 100%; background-color: {TEXTBOX_COLOR}')
+            ui.separator().style('background-color: #003366; height: 2px;')
 
     result_label = ui.label('')
 
